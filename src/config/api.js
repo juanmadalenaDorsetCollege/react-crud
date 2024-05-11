6bf37a92e0fad01ff5be0f90ee5a9c3a6618c8a2
juanmadalenaDorsetCollege <juandorsetcollege@gmail.com>
2024-05-11 23:41:25 +0200

    Update api.js

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://top-players-mongo.zeabur.app",
+    baseURL: "https://node-players-fkheol5d5-juans-projects-bfa47474.vercel.app",
 });
 
 export default api;
